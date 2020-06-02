01a1e54c2132239cac50d6bb9b2a84ee64777291
theredbe <->
2020-06-02 13:24:47 +0300

    created ability to switch between calculators

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
+
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    modules: []
-  }
 
-  componentWillMount() {
+
+class App extends React.Component {
+  constructor() {
+    super();
+
     window['registerModule'] = this.addModule.bind(this);
+
+    this.state = {
+      modules: [], 
+      currentModuleIndex: -1
+    }
   }
 
   addModule(module) {
     const modules = [...this.state.modules];
     modules.push(module);
+
     this.setState({modules});
   }
 
   render() {
-    return this.state.modules.map(m => <div>{m}</div>);
+    const { currentModuleIndex } = this.state;
+    const Module = this.state.modules[currentModuleIndex];
+
+    return (
+      <div className='container'>
+        <Button variant="contained" onClick={() => { this.setState({ currentModuleIndex: 0 }) }}>Калькулятор для вычисления площади треугольника</Button>
+
+        <Button variant="contained" onClick={() => { this.setState({ currentModuleIndex: 1 }) }}>Калькулятор для вычисления НОД</Button>
+        {Module && new Module().render()}
+      </div>
+    );
   }
 }
 
